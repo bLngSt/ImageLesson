8975fcf8f28c59cba781ce137d74bd77e77e7805
bLngSt <75664742+bLngSt@users.noreply.github.com>
2021-02-26 16:16:20 +0900

    Initial Commit

---
+++ ImageLesson/ImageLessonApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageLessonApp.swift
+//  ImageLesson
+//
+//  Created by にょにょ on 2021/02/26.
+//
+
+import SwiftUI
+
+@main
+struct ImageLessonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
