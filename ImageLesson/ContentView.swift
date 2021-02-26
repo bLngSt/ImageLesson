2448189c9ddfd4de0ff0052052e50388c44b236c
bLngSt <75664742+bLngSt@users.noreply.github.com>
2021-02-26 16:36:26 +0900

    画像の追加

--- ImageLesson/ContentView.swift
+++ ImageLesson/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Image("244f2adee9e159a17c404c3a0301e08e")
+                
+            VStack {
+                Text("Hello, world!")
+                    .padding()
+            }
+        }
     }
 }
 
